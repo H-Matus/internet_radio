160d74e5f2eb466931633f918a04b7ea0aeae18a
Henrikas <matusevicius.henrikas@gmail.com>
2020-05-25 22:32:42 +0100

    Updated setup to connect to WiFi.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 
 void setup() 
 {
+    Serial.begin(9600);
+    Serial.println();
+
+    delay(1000);
     
+    WiFi.begin("BeBox 3", "snowdrop396");
+
+    Serial.print("Connecting");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() 
 {
-    
+
 }
