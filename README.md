61fd309131d5ccdfc2dcfd8c7d3cf740eb16a7de
Henrikas <matusevicius.henrikas@gmail.com>
2020-06-13 20:02:53 +0100

    ISR Code Testing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # internet_radio
-This is a project to build an internet radio using NodeMCU microcontroller with preinstalled ESP8266.
+This is a project to build an internet radio using Espressif ESP32-WROOM-32D microcontroller with preinstalled ESP32S.
+
+Serial baudrate of ESP32 = 9600
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,65 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <DNSServer.h>
 
+struct Button
+{
+    const uint8_t PIN;
+    uint32_t numberKeyPresses;
+    bool pressed;
+};
+
+Button button1 = {18, 0, false};
+const int LedPin = 2;
+
+
+void IRAM_ATTR isr()
+{
+    button1.numberKeyPresses += 1;
+    button1.pressed = true;
+}
+
+void setup()
+{
+    // Setup
+    Serial.begin(9600);
+    delay(1000);
+    Serial.println("Setup complete");
+
+    // Assigning pins
+    pinMode(button1.PIN, INPUT_PULLUP);
+    pinMode(LedPin, OUTPUT);
+
+    // ISRs
+    attachInterrupt(button1.PIN, isr, FALLING);
+}
+
+void loop()
+{
+
+    if (button1.pressed)
+    {
+        Serial.printf("Button 1 has been pressed %u times\n", button1.numberKeyPresses);
+        button1.pressed = false;
+        digitalWrite(2, HIGH);
+        delay(500);
+        digitalWrite(2, LOW);
+    }
+
+    
+
+    //Detach Interrupt after 1 Minute
+    static uint32_t lastMillis = 0;
+    if (millis() - lastMillis > 60000)
+    {
+        lastMillis = millis();
+        detachInterrupt(button1.PIN);
+        Serial.println("Interrupt Detached!");
+    }
+}
+
+/*
 void setup() 
 {
     Serial.begin(9600);
@@ -21,8 +80,4 @@ void setup()
     Serial.print("Connected, IP address: ");
     Serial.println(WiFi.localIP());
 }
-
-void loop() 
-{
-
-}
+*/
