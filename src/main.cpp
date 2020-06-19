4e0c57ee0d4dc29a745fa25ef5f3b6b70b1f006d
Henrikas <matusevicius.henrikas@gmail.com>
2020-06-19 22:54:33 +0100

    Better comments, serial prints and a new radio.

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,10 @@
 #include <HTTPClient.h>
 #include <esp_wifi.h>
 #include <WiFi.h>
+#include <WebServer.h>
 
 // Custom Libraries
-
+#include "ServerHandling.h"
 
 // MP3 Decoder Pins
 #define VS1053_CS     5
@@ -14,39 +15,44 @@
 #define VS1053_DREQ   4
 
 // Default volume
-#define VOLUME 80
+#define VOLUME 70
 
+// Define MP3 decoder and WiFi client
 VS1053 player(VS1053_CS, VS1053_DCS, VS1053_DREQ);
 WiFiClient client;
 
-// WiFi settings example, substitute your own
+// Define WiFi name and password of the local network
 const char *ssid = "BeBox 3";
 const char *password = "snowdrop396";
 
-char *host = "realfm.live24.gr";
-char *path = "/realfm";
-int httpPort = 80;
+// Define host, path and HTTP Port of the main radio station
+char *host = "82.135.234.195"; // realfm.live24.gr
+char *path = "/kelyje_vilnius.mp3"; // /realfm
+int httpPort = 8000;
 
-// The buffer size 64 seems to be optimal. At 32 and 128 the sound might be brassy.
+// Define buffer size. 64 seems to be optimal. At 32 and 128 the sound might be brassy.
 uint8_t mp3buff[64];
 
 void setup()
 {
+    // Initialise serial bus
     Serial.begin(115200);
 
     // Wait for VS1053 and PAM8403 to power up
     // otherwise the system might not start up correctly
     delay(3000);
+    Serial.println("\n\nESP32 Wifi Radio");
 
-    Serial.println("\n\nSimple Radio Node WiFi Radio");
-
+    // Initialise SPI bus
     SPI.begin();
 
+    // Initialise MP3 decoder
     player.begin();
     player.switchToMp3Mode();
-    player.setVolume(100);
+    player.setVolume(VOLUME);
 
-    Serial.print("Connecting to SSID ");
+    // Connect to the local network
+    Serial.print("Connecting to local network\nSSID: ");
     Serial.println(ssid);
     WiFi.begin(ssid, password);
 
@@ -57,10 +63,14 @@ void setup()
     }
 
     Serial.println("WiFi connected");
-    Serial.println("IP address: ");
+    Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
 
-    Serial.print("connecting to ");
+    // Initialise ESP32 HTTP Server
+    server.on("/", handleOnConnect);
+    server.onNotFound(handleOnNotFound);
+
+    Serial.print("Connecting to ");
     Serial.println(host);
 
     if (!client.connect(host, httpPort))
@@ -77,6 +87,7 @@ void setup()
 
 void loop()
 {
+    // Reconnect, if lost connection
     if (!client.connected())
     {
         Serial.println("Reconnecting...");
@@ -86,6 +97,7 @@ void loop()
         }
     }
 
+    // Play the incoming bitstream, if there are availables bits
     if (client.available() > 0)
     {
         // The buffer size 64 seems to be optimal. At 32 and 128 the sound might be brassy.
